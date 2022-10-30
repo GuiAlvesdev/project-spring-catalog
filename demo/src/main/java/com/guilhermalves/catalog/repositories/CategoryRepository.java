e54ba16210857c9a339f41d8e001a73ca2be8cc9
GuiAlvesdev <guialves1353@gmail.com>
2022-10-29 23:38:30 -0300

    lets go, upload with h2 :fire:

---
+++ demo/src/main/java/com/guilhermalves/catalog/repositories/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.guilhermalves.catalog.repositories;
+
+import com.guilhermalves.catalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository  extends JpaRepository<Category, Long> {
+
+
+
+
+
+
+}
---
+++ demo/src/main/java/com/guilhermalves/catalog/services/CategoryService.java
@@ -0,0 +1,30 @@
+package com.guilhermalves.catalog.services;
+
+
+import com.guilhermalves.catalog.entities.Category;
+import com.guilhermalves.catalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll(){
+        return repository.findAll();
+
+    }
+
+
+
+
+
+
+}
