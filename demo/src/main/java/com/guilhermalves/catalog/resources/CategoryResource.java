aa0f810d07d07355142717c82ea266fd49ad2c6e
GuiAlvesdev <guialves1353@gmail.com>
2022-11-01 15:07:55 -0300

    att tests using postman :white_check_mark

--- demo/src/main/java/com/guilhermalves/catalog/resources/CategoryResource.java
+++ demo/src/main/java/com/guilhermalves/catalog/resources/CategoryResource.java
@@ -4,14 +4,13 @@ import com.guilhermalves.catalog.dto.CategoryDTO;
 import com.guilhermalves.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+
 
 
 @RestController
@@ -23,16 +22,9 @@ public class CategoryResource {
 
 
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable ){
 
-    ){
-            PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-            Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+            Page<CategoryDTO> list = service.findAllPaged(pageable);
 
             return ResponseEntity.ok().body(list);
 
