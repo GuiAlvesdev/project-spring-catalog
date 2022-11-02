6f7509dbd12f9e66809935fdfa61e14b268f9ce7
GuiAlvesdev <guialves1353@gmail.com>
2022-11-02 18:35:07 -0300

    custom validation mensages errors :working:

--- demo/src/main/java/com/guilhermalves/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ demo/src/main/java/com/guilhermalves/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.guilhermalves.catalog.services.exceptions.DatabaseException;
 import com.guilhermalves.catalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -44,9 +46,23 @@ public class ResourceExceptionHandler {
 
 
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request)
+    {
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        err.setError("Validation Exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()){
+            err.AddError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
 
 
-
-
+    }
 
 }
