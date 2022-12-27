972719a36bf51bcddc808c7497b8b69a5869795d
GuiAlvesdev <guialves1353@gmail.com>
2022-12-26 21:26:07 -0300

    realizando test repository JPA com junit :sparkes:

--- src/test/java/com/guialvesdev/catalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/guialvesdev/catalog/repositories/ProductRepositoryTests.java
@@ -3,7 +3,9 @@ package com.guialvesdev.catalog.repositories;
 
 import com.guialvesdev.catalog.entities.Product;
 import com.guialvesdev.catalog.repository.ProductRepository;
+import com.guialvesdev.catalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,13 +16,45 @@ import java.util.Optional;
 @DataJpaTest
 public class ProductRepositoryTests {
 
+    private long exintingfId = 1L;
+    private long nonExistingId = 1000L;
+
+    private long countTotalProducts = 25L;
+    
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        exintingfId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+
+
+    }
+
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdNull(){
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
+
+
+
+
+
     @Autowired
     private ProductRepository repository;
 
     @Test
     public void deleteShouldDeleteObjectIdExists(){
 
-        long exintingfId = 1L;
+
 
         repository.deleteById(exintingfId);
 
@@ -32,12 +66,15 @@ public class ProductRepositoryTests {
 
     @Test
     public void deleteShouldIdThrowEmptyResultDataAcessExceptionWhenIdNotExist(){
-        long nonExistingId = 1000L;
+
 
         Assertions.assertThrows(EmptyResultDataAccessException.class,() -> {
             repository.deleteById(nonExistingId);
         });
     }
+
+
+
 
 
 
---
+++ src/test/java/com/guialvesdev/catalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.guialvesdev.catalog.tests;
+
+import com.guialvesdev.catalog.dto.ProductDTO;
+import com.guialvesdev.catalog.entities.Category;
+import com.guialvesdev.catalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct(){
+        Product product = Product(1L, "Phone", "Goood Phone", 800.0, "Https://IMG", Instant.parse("2020-07-14T10:00:00Z"));
+        product.getCategories().add(new Category(2L, "Eletronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
