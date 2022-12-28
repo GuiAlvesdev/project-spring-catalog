96eaef89523fd211d4fa60be632957f8ee92a9f6
GuiAlvesdev <guialves1353@gmail.com>
2022-12-28 17:46:30 -0300

    adicionado excessao profile h2 para test

--- src/main/java/com/guialvesdev/catalog/config/ResourceServerConfig.java
+++ src/main/java/com/guialvesdev/catalog/config/ResourceServerConfig.java
@@ -33,15 +33,19 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String[] ADMIN = { "/users/**" };
 
+
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.tokenStore(tokenStore);
     }
 
+
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        // H2
+        // LIBERAR O PROFILE H2 PARA TEST
         if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
@@ -56,6 +60,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http.cors().configurationSource(corsConfigurationSource());
     }
 
+    
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
 
