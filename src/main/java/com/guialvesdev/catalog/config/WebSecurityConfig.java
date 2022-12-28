3f2b2d05ae86b97c13ca3546e8e355572b65a2e3
GuiAlvesdev <guialves1353@gmail.com>
2022-12-28 16:59:02 -0300

    adicionado tokenenhancer informacoes adicionais a resposta no corpo do token :security:

--- src/main/java/com/guialvesdev/catalog/config/WebSecurityConfig.java
+++ src/main/java/com/guialvesdev/catalog/config/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package com.guialvesdev.catalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 
+@Deprecated
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig  {
